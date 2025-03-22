626f2c60ca06605884fc9833ce4b9f593a3be977
guiijo <161329627+guiijo@users.noreply.github.com>
2025-03-22 21:43:48 +0000

    CodCesar, CodReverso e testes no App

--- App.java
+++ App.java
@@ -13,5 +13,32 @@ public class App {
         System.out.println("Texto original: "+texto);
         System.out.println("Texto codificado: "+codificado);
         System.out.println("Texto decodificado: "+decodificado);
+
+        // Teste com CodificadorCesar
+        Codificador codCesar = new CodificadorCesar(3); // Shift de 3
+        System.out.println("\nCodificador: " + codCesar.getNome());
+        System.out.println("Versão: " + codCesar.getDataCriacao());
+        System.out.println("Nível de segurança: " + codCesar.getNivelSeguranca());
+        
+        codificado = codCesar.codifica(texto);
+        decodificado = codCesar.decodifica(codificado);
+
+        System.out.println("\nTexto original: " + texto);
+        System.out.println("Texto codificado: " + codificado);
+        System.out.println("Texto decodificado: " + decodificado);
+
+        // Teste com CodificadorReverso
+        Codificador codReverso = new CodificadorReverso();
+        System.out.println("\nCodificador: " + codReverso.getNome());
+        System.out.println("Versão: " + codReverso.getDataCriacao());
+        System.out.println("Nível de segurança: " + codReverso.getNivelSeguranca());
+        
+        codificado = codReverso.codifica(texto);
+        decodificado = codReverso.decodifica(codificado);
+
+        System.out.println("\nTexto original: " + texto);
+        System.out.println("Texto codificado: " + codificado);
+        System.out.println("Texto decodificado: " + decodificado);
+    
     }
 }
